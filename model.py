ec689353700a8a33eb0a90d5cc19c8765da2cc61
zalastopar <50300925+zalastopar@users.noreply.github.com>
2019-08-04 16:42:29 +0200

    odpravila napake, ki sem jih imela z meniji in bolj ali manj dokončala

--- model.py
+++ model.py
@@ -8,6 +8,13 @@ class Igralec:
     def __str__(self):
         return 'Vaše stanje je {0} €'.format(self.stanje)
 
+
+def spremeni_stanje(igralec, hand, bonus):
+    if bonus == 0:
+        igralec.stanje -= hand.stava
+    else:
+        igralec.stanje += bonus
+
 class Karta:
 
     def __init__(self, vrednost, barva):
@@ -48,7 +55,7 @@ class Hand:
 def izloci_karte(hand, pozicija):
     roka = hand.roka
     for el in pozicija[::-1]:
-        del roka[el]
+        del roka[int(el) -  1]
     hand.roka = roka
     return hand
 
@@ -61,6 +68,8 @@ def dodaj_karte(hand):
 
 def preveri_ce_je_stevilka(stevilka):
     st_pik = 0
+    if len(stevilka) == 0:
+        return False
     for el in stevilka:
         if el in '1234567890':
             pass
@@ -69,11 +78,16 @@ def preveri_ce_je_stevilka(stevilka):
                 if st_pik == 0:
                     st_pik += 1
                 else:
-                    return False
+                    return 'ne'
             else:
-                return False
-    return True
+                return 'ne'
+    return 'ja'
 
+def preveri_ce_je_dovolj_denarja(igralec, denar):
+    if igralec.stanje - float(denar) >= 0:
+        return True
+    else:
+        return False
 
 def naredi_igralca(denar):
     igralec = Igralec(float(denar))
@@ -82,6 +96,156 @@ def naredi_igralca(denar):
 def nova_roka(denar):
     roka = Hand(float(denar))
     return roka
+
+def preveri_ce_so_karte_pravilno_vnesene(karte):
+    if len(karte) == 0:
+        return False
+    for el in karte:
+        if el in '12345' and karte.count(el) == 1:
+            return True
+        else:
+            return False
+
+def preveri_ja_ali_ne(odgovor):
+    odgovor = odgovor.lower()
+    if odgovor == 'ja' or odgovor == 'da':
+        return 'ja'
+    elif odgovor == 'ne':
+        return 'ne'
+    elif len(odgovor) == 0:
+        return 'narobe'
+    else:
+        return 'narobe'
+    
+def preveri_par(hand):
+    sez_vrednosti = []
+    for el in hand.roka:
+        sez_vrednosti += [el.vrednost]
+    for vr in sez_vrednosti:
+        if sez_vrednosti.count(vr) == 2 and vr in 'JQKA':
+            return True
+    return False
+
+
+def preveri_dva_para(hand):
+    sez_vrednosti = []
+    st_parov = 0
+    for el in hand.roka:
+        sez_vrednosti += [el.vrednost]
+    for el in sez_vrednosti:
+        st = sez_vrednosti.count(el)
+        if st == 2:
+            st_parov += 1
+    return (st_parov / 2) == 2
+    
+def preveri_tris(hand):
+    sez_vrednosti = []
+    for el in hand.roka:
+        sez_vrednosti += [el.vrednost]
+    for vr in sez_vrednosti:
+        if sez_vrednosti.count(vr) == 3:
+            return True
+    return False
+
+def spremeni_slikice_v_stevilke(sez):
+    for el in sez:
+        if el == 'J':
+            sez = sez[0 : sez.index(el)] + ['11'] + sez[sez.index(el) + 1:]
+        elif el == 'Q':
+            sez = sez[0 : sez.index(el)] + ['12'] + sez[sez.index(el) + 1:]
+        elif el == 'K':
+            sez = sez[0 : sez.index(el)] + ['13'] + sez[sez.index(el) + 1:]
+        elif el == 'A':
+            sez = sez[0 : sez.index(el)] + ['14'] + sez[sez.index(el) + 1:]
+        elif el == 'T':
+            sez = sez[0 : sez.index(el)] + ['10'] + sez[sez.index(el) + 1:]
+    return sez
+
+def razvrsti_po_velikosti(sez):
+    nov_sez = []
+    for el in sez:
+        nov_sez += [int(el)]
+    return sorted(nov_sez)
+
+def preveri_lestvico(hand):
+    sez_vrednosti = []
+    for el in hand.roka:
+        sez_vrednosti += [el.vrednost]
+    sez_vrednosti = spremeni_slikice_v_stevilke(sez_vrednosti)
+    sez_vrednosti = razvrsti_po_velikosti(sez_vrednosti)
+    zacetek = int(sez_vrednosti[0])
+    return (zacetek + 1) in sez_vrednosti and (zacetek + 2) in sez_vrednosti and (zacetek + 3) in sez_vrednosti and (zacetek + 4) in sez_vrednosti
+
+def preveri_barvo(hand):
+    sez_barv = []
+    for el in hand.roka:
+        sez_barv += [el.barva]
+    return sez_barv.count(sez_barv[0]) == 5 
+
+def preveri_full_house(hand):
+    sez_vrednosti = []
+    for el in hand.roka:
+        sez_vrednosti += [el.vrednost]
+    for vr in sez_vrednosti:
+        if str(sez_vrednosti.count(vr)) not in '23':
+            return False
+    return True
+
+def preveri_poker(hand):
+    sez_vrednosti = []
+    for el in hand.roka:
+        sez_vrednosti += [el.vrednost]
+    return sez_vrednosti.count(sez_vrednosti[0]) == 5 
+
+def preveri_barvno_lestvico(hand):
+    return preveri_barvo(hand) and preveri_lestvico(hand)
+
+def preveri_royal_flush(hand):
+    sez_vrednosti = []
+    for el in hand.roka:
+        sez_vrednosti += [el.vrednost]
+    return preveri_barvo(hand) and 'T' in sez_vrednosti and 'J' in sez_vrednosti and 'Q' in sez_vrednosti and 'K' in sez_vrednosti and 'A' in sez_vrednosti
+    
+
+def doloci_bonus(hand):
+    stava = hand.stava
+    if preveri_par(hand):
+        return stava
+    elif preveri_dva_para(hand):
+        return stava * 2
+    elif preveri_tris(hand):
+        return stava * 3
+    elif preveri_lestvico(hand):
+        return stava * 4
+    elif preveri_barvo(hand):
+        return stava * 5
+    elif preveri_full_house(hand):
+        return stava * 8
+    elif preveri_poker(hand):
+        return stava * 25
+    elif preveri_barvno_lestvico(hand):
+        return stava * 50
+    elif preveri_royal_flush(hand):
+        return stava * 250
+    else:
+        return 0
+
+def preveri_ce_je_ena_ali_dva(odgovor):
+    if odgovor == '1':
+        return 'ena'
+    elif odgovor == '2':
+        return 'dva'
+    elif len(odgovor) == 0:
+        return 'narobe'
+    else:
+        return 'narobe'
+    
+def preveri_koliko_denarja(igralec):
+    denar = igralec.stanje
+    if denar == 0:
+        return True
+    else:
+        return False
 
 
 
--- tekstovni_umesnik.py
+++ tekstovni_umesnik.py
@@ -1,6 +1,6 @@
 import model
 from model import Igralec, Karta, Hand
-
+import time
 
 
 
@@ -8,39 +8,135 @@ from model import Igralec, Karta, Hand
 def polog_denarja():
     print('Koliko denarja želite položiti za mizo?')
     denar = input('> ')
-    if model.preveri_ce_je_stevilka(denar):
+    if model.preveri_ce_je_stevilka(denar) == 'ja':
         igralec = model.naredi_igralca(denar)
         return igralec
     else:
         print('Vaša izbira je neveljavna!')
-        polog_denarja()
+        return polog_denarja()
 
-def stava():
+def stava(igralec):
     print('Koliko denarja želite staviti?')
     denar = input('> ')
-    if model.preveri_ce_je_stevilka(denar):
-        roka = model.nova_roka(denar)
-        return roka
+    if model.preveri_ce_je_stevilka(denar) == 'ja':
+        if model.preveri_ce_je_dovolj_denarja(igralec, denar):
+            roka = model.nova_roka(denar)
+            return roka
+        else: 
+            print('Na vašem računu ni dovolj denarja!')
+            return stava(igralec)
     else:
         print('Vaša izbira je neveljavna!')
-        stava()
+        return stava(igralec)
 
+def zamenjaj_karte(roka):
+    print('Zapišite mesta kart, ki jih želite zamenjati!')
+    print('Zapišite jih, kot število.')
+    pozicija = input('> ')
+    if model.preveri_ce_so_karte_pravilno_vnesene(pozicija):
+        roka = model.izloci_karte(roka, pozicija)
+        model.dodaj_karte(roka)
+        print(roka)
+    else:
+        print('Vaša izbira je neveljavna!')
+        zamenjaj_karte(roka)
+
+
+def ali_zelite_menjati_karte(roka):
+    print('Ali želite zamenjati katero od svojih kart?')
+    odgovor = input('> ')
+    odgovor = model.preveri_ja_ali_ne(odgovor)
+    if odgovor == 'ja':
+        zamenjaj_karte(roka)
+    elif odgovor == 'ne':     
+        pass
+    else:
+        print('Vaša izbira je neveljavna!')
+        ali_zelite_menjati_karte(roka)
+
+def ali_zelite_nadaljevati_igro(igralec):
+    print('Ali želite ponovno staviti?')
+    odgovor = input('> ')
+    odgovor = model.preveri_ja_ali_ne(odgovor)
+    if odgovor == 'ja':
+        igra(igralec)
+    elif odgovor == 'ne':     
+        print('Hvala za igro!')
+        return False
+    else:
+        print('Vaša izbira je neveljavna!')
+        ali_zelite_nadaljevati_igro(igralec)
     
+def zmanjkalo_denarja():
+    print('Zmanjkalo vam je denarja.')
+    print('Ali želite še kaj denarja naložiti?')
+    odgovor = input('> ')
+    odgovor = model.preveri_ja_ali_ne(odgovor)
+    if odgovor == 'ja':
+        igralec = polog_denarja()
+        igra(igralec)
+    elif odgovor == 'ne':    
+        print('Hvala za igro!')
+        return False
+    else:
+        print('Vaša izbira je neveljavna!')
+        zmanjkalo_denarja()
 
 
+def igra(igralec):
+    roka = stava(igralec)
+    print(roka)
+    ali_zelite_menjati_karte(roka)
+    bonus = model.doloci_bonus(roka)
+    print('Vaše karte so vam pridesle ' + str(bonus) + ' €')
+    model.spremeni_stanje(igralec, roka, bonus)
+    print(igralec)
+    if model.preveri_koliko_denarja(igralec):
+        odgovor = zmanjkalo_denarja()
+        return odgovor
+    else:
+        odgovor = ali_zelite_nadaljevati_igro(igralec)
+        return odgovor
 
-def igra():
-    stava()
+def pokazi_navodila():
+    with open('navodila.txt', 'r', encoding = 'utf-8') as pravila:
+        for vrstica in pravila:
+            print(vrstica[:-1])
+        
+
+def zacetni_meni():
+    print('Kaj želite storiti?')
+    print('1) Moram pokukati v navodila')
+    print('2) Želim igrati!')
+    odgovor = input('> ')
+    odgovor = model.preveri_ce_je_ena_ali_dva(odgovor)
+    if odgovor == 'ena':
+        pokazi_navodila()
+        time.sleep(5)
+        zacetni_meni()
+    elif odgovor == 'dva':
+        pass
+    else:
+        print('Vaš odgovor je neveljaven!')
+        zacetni_meni()
+    
 
 
 def intro():
     print('Pozdravljeni v moji bedni igri!')
-    print('Porabite čim več denarja ker ga dobim jaz')
-    polog_denarja()
+    print('Porabite čim več denarja, ker ga dobim jaz.')
+    zacetni_meni()
+    igralec = polog_denarja()
+    return igralec
+    
+
 
 def main():
-    intro()
+    igralec = intro()
     while True:
-        igra()
+        if not igra(igralec):
+            break
+        else:
+            continue
 
 main()
